e9c177f53c5043dc5bf1e056cce75366a315b659
Abhijit Bharali <abhi_8@rocketmail.com>
2021-12-30 10:23:18 +0530

    Add files via upload

---
+++ my_math.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 16 15:29:54 2021
+
+@author: abhijit.bharali
+"""
+
+import numpy as np
+
+def add(a,b):
+    return a+b
+
+def mult(a,b):
+    return a*b
+
+x = 30
+
+def gump(a):
+    b = np.array(a)
+    return list(b*10)
+
+
+#a, b = list(np.arange(10)), np.array([1,2,3,4,5]).tolist()
+
